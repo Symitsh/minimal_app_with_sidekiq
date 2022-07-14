3385ca846bfa9df67478d502c40dc3cc2d3a5ea6
Symitsh <sandri822@hotmail.fr>
2022-07-14 09:09:17 +0200

    ✅ Call job from view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   get "welcome/index"
+
+  # route où tout visiteur a besoin que le helloWorldJob soit déclenché
+  post "welcome/trigger_job"
+
+  # où les visiteurs sont redirigés une fois que job a été appelé
+  get "other/job_done"
+
   root to: "welcome#index"
 end
